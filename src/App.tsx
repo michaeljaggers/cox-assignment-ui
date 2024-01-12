73484bf19c025c7317f3c974fee1ff4d09b54a0a
michaeljaggers <michael.jaggers@gmail.com>
2024-01-12 14:58:49 -0600

    working bootstrap card

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
 import React from 'react';
 import './styles/App.scss';
+import CustomCard from './components/CustomCard';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <CustomCard
+          heading='Test heading'
+          subheading='subheading'
+          background='background'
+          image='image'
+          price={5000}
+        />
       </header>
     </div>
   );
--- src/components/CustomCard.tsx
+++ src/components/CustomCard.tsx
@@ -2,22 +2,32 @@ import React from "react";
 import Card from 'react-bootstrap/Card';
 
 interface CardProps {
-
+    heading: string;
+    subheading: string;
+    background: string;
+    image: string;
+    price: number;
 };
 
-const CustomCard = (props: CardProps) => {
+const CustomCard = ({
+    heading,
+    subheading,
+    background,
+    image,
+    price
+}: CardProps) => {
 
     return (
         <Card style={{ width: '18rem' }}>
             <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                <Card.Title>{heading}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">{subheading}</Card.Subtitle>
                 <Card.Text>
                     Some quick example text to build on the card title and make up the
                     bulk of the card's content.
                 </Card.Text>
-                <Card.Link href="#">Card Link</Card.Link>
-                <Card.Link href="#">Another Link</Card.Link>
+                <Card.Link href="#">{price}</Card.Link>
+                
             </Card.Body>
         </Card>
     );
